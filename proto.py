d7cf0a36a4948535cbf3908542b881cee38aaeab
albini <albert.2010@live.ca>
2020-12-18 11:30:05 -0800

    created gitListVersions

--- proto.py
+++ proto.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+from git import Repo
 
 #returns a recursive list with all the files and sub-files inside a folder
 #Ex.:
@@ -38,9 +39,23 @@ def listJavaFiles(filesArray):
             resultat.append(file)
     return resultat
 
+def gitListVersions(url,path):
+
+	tempPath = path+"/temp"
+	repo = Repo.clone_from(url, tempPath)
+	resultat = list(repo.iter_commits("MASTER"))
+	shutil.rmtree(tempPath)
+	return resultat
+
+
 if __name__ == "__main__":
 
-    target = sys.argv[1]
-    list = listFiles(target)
-    result = listJavaFiles(list)
+    #target = sys.argv[1]
+    #list = listFiles(target)
+    #result = listJavaFiles(list)
+
+    url = sys.argv[1]
+    path = sys.argv[2]
+    result = gitListVersions(url,path)
+
     print(result)
